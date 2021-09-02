59483f04bb1629fb9c0fb294dbeb573c91557fb7
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-09-02 16:11:11 +0600

    compleet assignment

--- js/main.js
+++ js/main.js
@@ -1,73 +1,84 @@
-// load data here
+// ------------------------------load data function------------------------------
 const loadData = async () => {
   // get input text
   const searchInput = document.getElementById('search');
   const searchText = searchInput.value;
 
-  // error handling
+  //empty input error handling -->
   if (searchText === '') {
     document.getElementById('error-div').innerHTML = `
-    <h2 class="text-center bg-warning text-danger p-3 rounded-3">Search field cannot be empty.</h2>
+    <h4 class="text-center bg-warning text-danger p-3 rounded-3">Search field cannot be empty.</h4>
     `;
     return;
   }
 
-  //spinner on
+  //spinner on -->
   spinner.classList.remove('d-none');
 
-  // clear
+  // clear content -->
   searchInput.value = '';
   document.getElementById('error-div').textContent = '';
   document.getElementById('total-result').textContent = '';
   document.getElementById('book-container').textContent = '';
 
-  // fetch data
+  // fetch data from api -->
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
 
-  // error handling
+  // error handling for un-valid user input -->
   if (data.numFound === 0) {
     document.getElementById('error-div').innerHTML = `
-    <h2 class="text-center bg-warning text-danger p-3 rounded-3">No result found!</h2>
+    <h4 class="text-center bg-warning text-danger p-3 rounded-3">No result found!</h4>
     `;
   } else {
     document.getElementById('error-div').innerHTML = '';
   }
 
-  showData(data.docs);
+  // call showData function -->
+  showData(data);
 };
 
-const showData = (bookList) => {
-  // spinner of
+// ---------------------show Data Function---------------------
+const showData = (data) => {
+  // spinner of  -->
   spinner.classList.add('d-none');
 
-  // total result show
-  document.getElementById('total-result').innerText = `${bookList.length} book found`;
+  // total search result show -->
+  document.getElementById('total-result').innerText = `${data.numFound} books are available!`;
 
+  // result show
   const bookContainer = document.getElementById('book-container');
+  const bookList = data.docs;
 
   bookList.forEach((book) => {
     const div = document.createElement('div');
     div.classList.add('col');
-
     div.innerHTML = `
     <div class="card">
-        <img class="img-thumbnail" src="https://covers.openlibrary.org/b/id/${
+    
+        <img style="height: 300px;" class="img-thumbnail" src="https://covers.openlibrary.org/b/id/${
           book.cover_i
         }-M.jpg" alt="..." />
-        <div class="card-body">
-            <h5 class="card-title">Book Name:${book.title}</h5>
-            <p class="card-text">Writer: 
-            <span class="fw-bold">${book.author_name[0] ? book.author_name[0] : 'not available'}</span>
+
+        <div class="card-body " >
+
+            <h5 class="card-title ">Book Name:${book.title}</h5>
+
+            <p class="card-text fw-bolder text-primary">Writer: 
+            <span class="fw-bold text-dark">${book.author_name ? book.author_name[0] : ''}</span>
             </p>
-            <p class="card-text">First-Published: 
-            <span class="fw-bold">${book.first_publish_year}</span>
+
+            <p class="card-text fw-bolder text-primary">First-Published: 
+            <span class="fw-bold text-dark">${book.first_publish_year ? book.first_publish_year : ''}</span>
             </p>
-            <p class="card-text">Publisher: 
-            <span class="fw-bold">${book.publisher[0] ? book.publisher[0] : 'not available'}</span>
+
+            <p class="card-text fw-bolder text-primary">Publisher: 
+            <span class="fw-bold text-dark">${book.publisher ? book.publisher[0] : ''}</span>
             </p>  
+
         </div>
+
     </div>
     `;
     bookContainer.appendChild(div);
